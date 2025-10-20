bd0782e60e32891d8ae3643ef3774c706768e1f8
CeciliaAvila <cecilia.avila@southworks.com>
2025-10-20 12:05:49 -0300

    get appId from defaultConnection in agentResponseHandler

--- packages/agents-hosting/src/agent-client/agentResponseHandler.ts
+++ packages/agents-hosting/src/agent-client/agentResponseHandler.ts
@@ -58,7 +58,9 @@ const handleResponse = (adapter: CloudAdapter, handler: ActivityHandler, convers
 
   logger.debug('received response: ', activity)
 
-  const myTurnContext = new TurnContext(adapter, activity, CloudAdapter.createIdentity('myAppId'))
+  const appId = adapter.connectionManager.getDefaultConnection().connectionSettings?.clientId ?? ''
+
+  const myTurnContext = new TurnContext(adapter, activity, CloudAdapter.createIdentity(appId))
   const conversationDataAccessor = conversationState.createProperty<ConversationReferenceState>(req.params!.conversationId)
   const conversationRefState = await conversationDataAccessor.get(myTurnContext, undefined, { channelId: activity.channelId!, conversationId: req.params!.conversationId })
 
