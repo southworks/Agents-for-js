21086da4c06725c281eb553127e20a00d60ec961
CeciliaAvila <cecilia.avila@southworks.com>
2026-01-16 15:56:57 -0300

    Add OTelAgent sample

---
+++ test-agents/otelAgent/src/agentTelemetry.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+export class AgentTelemetry {
+  // TODO: Implement counters and histograms:
+  // MessageProcessedCounter
+  // RouteExecutedCounter
+  // MessageProcessingDuration
+  // RouteExecutionDuration
+  // ActiveConversations
+}
