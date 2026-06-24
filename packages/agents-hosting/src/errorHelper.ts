c66347a64041a5b878f83eb9cf0399f2a664975e
CeciliaAvila <cecilia.avila@southworks.com>
2026-06-24 10:48:32 -0300

    test closed issue

--- packages/agents-hosting/src/errorHelper.ts
+++ packages/agents-hosting/src/errorHelper.ts
@@ -633,6 +633,14 @@ export const Errors: { [key: string]: AgentErrorDefinition } = {
   },
 
   /**
+   * Error thrown when missing agent client config for specified agent.
+   */
+  MissingAgentClientConfig8: {
+    code: -120607,
+    description: 'Missing agent client config for agent {agentName}'
+  },
+
+  /**
      * Error thrown when agent name is required.
      */
   AgentNameRequired: {
